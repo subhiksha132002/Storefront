062bb5165cc1aaf4f0bb37f5d5a225d315670eef
Subhiksha B <subhiksha132002@gmail.com>
2025-07-11 21:55:38 +0530

    Created nested routers for reviews

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 from . import views
 
-router = SimpleRouter()
-router.register('products',views.ProductViewSet)
-router.register('collections',views.CollectionViewSet)
 
-urlpatterns = router.urls
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet, basename='products')
+router.register('collections', views.CollectionViewSet)
+
+products_router = routers.NestedDefaultRouter(
+    router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet,
+                         basename='product-reviews')
+urlpatterns = router.urls + products_router.urls
