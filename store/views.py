70da728a23f97ea8003e96b7c951c18fb99862e3
Subhiksha B <subhiksha132002@gmail.com>
2025-07-11 20:54:21 +0530

    Viewsets implemented fro products and collections

--- store/views.py
+++ store/views.py
@@ -1,40 +1,28 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import  api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
 from .models import Product,Collection
 from django.db.models import Count
 from .serializers import ProductSerializer,CollectionSerializer
-from rest_framework import status
 
-
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related('collection').all()
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def get_serializer_context(self):
         return {'request':self.request}
     
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
     def delete(self,request,pk ):
         product = get_object_or_404(Product, pk=pk)
         if product.orderitems.count() > 0:
             return Response({'error': 'Product cannot be deleted because it is associated with an orderitem.'})
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class CollectionList(ListCreateAPIView):
-     queryset = Collection.objects.annotate(products_count=Count('products')).all()
-     serializer_class = CollectionSerializer
-        
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.annotate(
-            products_count=Count('products'))
+  
+class CollectionViewSet(ModelViewSet):
+    queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class = CollectionSerializer
 
     def delete(self,request,pk):
@@ -43,3 +31,8 @@ class CollectionDetail(RetrieveUpdateDestroyAPIView):
             return Response({"error":'Collection cannot be deleted because it includes one or more products.'})
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+   
+
+    
