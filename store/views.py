05ba1e7bb77febca48c4f664fefe541dee393a95
Subhiksha B <subhiksha132002@gmail.com>
2025-07-12 09:02:28 +0530

    Pagination applied for ProductViewSet

--- store/views.py
+++ store/views.py
@@ -5,16 +5,18 @@ from rest_framework.filters import SearchFilter,OrderingFilter
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-
-from store.filters import ProductFilter
 from .models import OrderItem, Product,Collection,Review
 from .serializers import ProductSerializer,CollectionSerializer,ReviewSerializer
+from .filters import ProductFilter
+from .pagination import DefaultPagination
+
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
     filterset_class = ProductFilter
+    pagination_class = DefaultPagination
     search_fields = ['title','description']
     ordering_fields = ['unit_price','last_update']
 
