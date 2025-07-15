023c40d0b62bfeea726b20eb61c175c77d6a051c
Subhiksha B <subhiksha132002@gmail.com>
2025-07-15 08:32:20 +0530

    Rewriting data model for shopping cart

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from uuid import uuid4
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
@@ -90,12 +91,16 @@ class Address(models.Model):
     Customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE,related_name='items')
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = [['cart','product']]
 
 class Review(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='reviews')
