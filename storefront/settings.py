193e2ac18d57d02f8353f9f2c3ce78d60de666f4
Subhiksha B <subhiksha132002@gmail.com>
2025-07-16 09:48:17 +0530

    Created user profiles

--- storefront/settings.py
+++ storefront/settings.py
@@ -34,9 +34,9 @@ INSTALLED_APPS = [
 
     'playground',
     'store',
-    'store_custom',
     'tags',
     'likes',
+    'core',
     'rest_framework',
     'debug_toolbar',
     'django_filters',
@@ -143,3 +143,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False
 }
+
+AUTH_USER_MODEL = 'core.User'
