8152c2ba3597ad91df70df9cd11aec959ec3a210
Subhiksha B <subhiksha132002@gmail.com>
2025-07-18 10:09:15 +0530

    Built Profile API

--- store/views.py
+++ store/views.py
@@ -5,9 +5,9 @@ from rest_framework.filters import SearchFilter,OrderingFilter
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework.mixins import CreateModelMixin,RetrieveModelMixin,DestroyModelMixin
-from .models import OrderItem, Product,Collection,Review,Cart,CartItem
-from .serializers import ProductSerializer,CollectionSerializer,ReviewSerializer,CartSerializer,CartItemSerializer,AddCartItemSerializer,UpdateCartItemSerializer
+from rest_framework.mixins import CreateModelMixin,RetrieveModelMixin,DestroyModelMixin,UpdateModelMixin
+from .models import OrderItem, Product,Collection,Review,Cart,CartItem,Customer
+from .serializers import ProductSerializer,CollectionSerializer,ReviewSerializer,CartSerializer,CartItemSerializer,AddCartItemSerializer,UpdateCartItemSerializer,CustomerSerializer
 from .filters import ProductFilter
 from .pagination import DefaultPagination
 
@@ -76,4 +76,8 @@ class CartItemViewSet(ModelViewSet):
         return CartItem.objects\
             .filter(cart_id=self.kwargs['cart_pk'])\
             .select_related('product')
+    
+class CustomerViewSet(CreateModelMixin,RetrieveModelMixin,UpdateModelMixin,GenericViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
  
